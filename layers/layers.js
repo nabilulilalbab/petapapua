5f13c2aafb2a4837d1bde9438c33292244933ad5
Nabil Ulil Albab <nabilulilalbab78@gmail.com>
2025-11-18 21:43:47 +0700

    peta

---
+++ layers/layers.js
@@ -0,0 +1,39 @@
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_LapakGIS_Batas_Kabupaten_2024_1 = new ol.format.GeoJSON();
+var features_LapakGIS_Batas_Kabupaten_2024_1 = format_LapakGIS_Batas_Kabupaten_2024_1.readFeatures(json_LapakGIS_Batas_Kabupaten_2024_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_LapakGIS_Batas_Kabupaten_2024_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_LapakGIS_Batas_Kabupaten_2024_1.addFeatures(features_LapakGIS_Batas_Kabupaten_2024_1);
+var lyr_LapakGIS_Batas_Kabupaten_2024_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_LapakGIS_Batas_Kabupaten_2024_1, 
+                style: style_LapakGIS_Batas_Kabupaten_2024_1,
+                popuplayertitle: 'LapakGIS_Batas_Kabupaten_2024',
+                interactive: true,
+    title: 'LapakGIS_Batas_Kabupaten_2024<br />\
+    <img src="styles/legend/LapakGIS_Batas_Kabupaten_2024_1_0.png" /> 31 - 41.8<br />\
+    <img src="styles/legend/LapakGIS_Batas_Kabupaten_2024_1_1.png" /> 41.8 - 45.2<br />\
+    <img src="styles/legend/LapakGIS_Batas_Kabupaten_2024_1_2.png" /> 45.2 - 59<br />' });
+
+lyr_OpenStreetMap_0.setVisible(true);lyr_LapakGIS_Batas_Kabupaten_2024_1.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_LapakGIS_Batas_Kabupaten_2024_1];
+lyr_LapakGIS_Batas_Kabupaten_2024_1.set('fieldAliases', {'kabupaten': 'kabupaten', 'provinsi': 'provinsi', '2022_makanan2022': '2022_makanan2022', '2022_bukanmakanan2022': '2022_bukanmakanan2022', '2023_makanan2023': '2023_makanan2023', '2023_bukanmakanan2023': '2023_bukanmakanan2023', '2024_makanan2024': '2024_makanan2024', '2024_bukanmakanan2024': '2024_bukanmakanan2024', });
+lyr_LapakGIS_Batas_Kabupaten_2024_1.set('fieldImages', {'kabupaten': 'TextEdit', 'provinsi': 'TextEdit', '2022_makanan2022': 'TextEdit', '2022_bukanmakanan2022': 'TextEdit', '2023_makanan2023': 'TextEdit', '2023_bukanmakanan2023': 'TextEdit', '2024_makanan2024': 'TextEdit', '2024_bukanmakanan2024': 'TextEdit', });
+lyr_LapakGIS_Batas_Kabupaten_2024_1.set('fieldLabels', {'kabupaten': 'inline label - always visible', 'provinsi': 'inline label - always visible', '2022_makanan2022': 'inline label - always visible', '2022_bukanmakanan2022': 'inline label - always visible', '2023_makanan2023': 'inline label - always visible', '2023_bukanmakanan2023': 'inline label - always visible', '2024_makanan2024': 'inline label - always visible', '2024_bukanmakanan2024': 'inline label - always visible', });
+lyr_LapakGIS_Batas_Kabupaten_2024_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/LapakGIS_Batas_Kabupaten_2024_1_style.js
@@ -0,0 +1,47 @@
+var size = 0;
+var placement = 'point';
+
+var style_LapakGIS_Batas_Kabupaten_2024_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("2024_bukanmakanan2024");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("kabupaten") !== null) {
+        labelText = String(feature.get("kabupaten"));
+    }
+    if (value >= 31.040000 && value <= 41.840000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(247,251,255,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 41.840000 && value <= 45.210000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(115,178,216,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 45.210000 && value <= 58.950000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(8,48,107,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
